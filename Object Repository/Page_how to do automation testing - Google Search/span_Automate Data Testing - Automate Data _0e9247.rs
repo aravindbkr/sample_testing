59e43e8b75299007e65dcd7f5dbd27cf32cbcb4e
AravindBHASKAR <AravindBHASKAR@DESKTOP-C5B5QKF>
2022-11-09 14:17:43 +0530

    sample project

---
+++ Object Repository/Page_how to do automation testing - Google Search/span_Automate Data Testing - Automate Data _0e9247.rs
@@ -0,0 +1,99 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>span_Automate Data Testing - Automate Data _0e9247</name>
+   <tag></tag>
+   <elementGuidId>ccd45ca1-289a-4576-9497-1db4f5224263</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>div.CCgQ5.vCa9Yd.QfkTvb.MUxGbd.v0nnCb > span</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='tads']/div/div/div/div/div/a/div/span</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>span</value>
+      <webElementGuid>1b7c5c83-cfbf-45e6-9cb8-813be24afefd</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Automate Data Testing - Automate Data Quality Testing</value>
+      <webElementGuid>d78e6b2d-b9c9-49ca-b434-ce065ad581d8</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;tads&quot;)/div[@class=&quot;uEierd&quot;]/div[1]/div[1]/div[@class=&quot;vdQmEd fP1Qef EtOod pkphOe&quot;]/div[@class=&quot;v5yQqb&quot;]/a[@class=&quot;sVXRqc&quot;]/div[@class=&quot;CCgQ5 vCa9Yd QfkTvb MUxGbd v0nnCb&quot;]/span[1]</value>
+      <webElementGuid>d92b6fa2-1641-4d9a-8acf-7deb5c8f907e</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='tads']/div/div/div/div/div/a/div/span</value>
+      <webElementGuid>971748ce-13c0-4111-9829-0053f63dc073</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Ads'])[1]/following::span[1]</value>
+      <webElementGuid>fa698b25-22fb-46a1-a3d1-4cbb68d76f56</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Search help'])[1]/following::span[2]</value>
+      <webElementGuid>5a6a68f8-6dad-44ef-81f6-d174a0f600ff</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='·'])[1]/preceding::span[1]</value>
+      <webElementGuid>bdd89df4-52ba-4cdf-adcd-d36836f76e2d</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Automate Data Testing - Automate Data Quality Testing']/parent::*</value>
+      <webElementGuid>24a8dcd2-6cc5-4032-809e-350f11b44d1d</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[2]/div/div/div/div/div/div/a/div/span</value>
+      <webElementGuid>8bee40ef-6792-47a2-836c-88ea424603d0</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//span[(text() = 'Automate Data Testing - Automate Data Quality Testing' or . = 'Automate Data Testing - Automate Data Quality Testing')]</value>
+      <webElementGuid>d1683782-3bda-4ae7-9dfe-4cd7ad1c9f4f</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
